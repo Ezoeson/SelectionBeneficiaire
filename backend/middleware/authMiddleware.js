11ba8b3871bcb13bdbac7b5c3ab9b8a1f38f4896
Ezoeson <nambinintsoa6017@gmail.com>
2023-11-17 16:34:53 +0300

    condition if avec cookie

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,36 +4,39 @@ const prisma = new PrismaClient();
 import jwt from 'jsonwebtoken';
 
 const protect = asyncHandler(async (req, res, next) => {
-  const token = req.headers?.cookie?.split(';')[2].split("=")[1]
-  console.log(token);
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log(decoded)
-
-      const user = await prisma.compte.findMany({
-        where: {
-          clerkId: decoded.sub,
-        },
-        select: {
-          isAdmin: true,
-          clerkId: true,
-        },
-      });
-      if (user?.length > 0) {
-        req.user = user[0];
-        next();
-      } else {
+  if(req.headers && req.headers.cookie) {
+    const token = req.headers?.cookie?.split(';')[2].split("=")[1]
+    console.log(token);
+    if (token) {
+      try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decoded)
+  
+        const user = await prisma.compte.findMany({
+          where: {
+            clerkId: decoded.sub,
+          },
+          select: {
+            isAdmin: true,
+            clerkId: true,
+          },
+        });
+        if (user?.length > 0) {
+          req.user = user[0];
+          next();
+        } else {
+          res.status(401);
+          throw new Error('Not authorized,user not found');
+        }
+      } catch (error) {
         res.status(401);
-        throw new Error('Not authorized,user not found');
+        throw new Error('Not authorized, token failed');
       }
-    } catch (error) {
+    } else {
       res.status(401);
-      throw new Error('Not authorized, token failed');
+      throw new Error('Not authorized, no token');
     }
-  } else {
-    res.status(401);
-    throw new Error('Not authorized, no token');
+
   }
 });
 // User must be an admin
